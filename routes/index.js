14d1b3ec4127733cfcc5b9c9a63ebcab73a361df
dbarks1024 <dbarker@bignerdranch.com>
2018-11-09 12:50:12 -0500

    Add delete post

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,16 @@ router.post('/post', (req, res) => {
   });
 });
 
+router.delete('/post/:id', (req, res) => {
+  Post.findByIdAndDelete(req.params.id, (err) => {
+    if(err) {
+      res.send(err);
+    } else{
+      res.send('success');
+    }
+  });
+});
+
 router.get('/post', (req, res) => {
   Post.find((err, foundPosts) => {
     res.json(foundPosts);
