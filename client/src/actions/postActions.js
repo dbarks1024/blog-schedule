4daae38acc65838d81afbdc98739814cb7d6c161
dbarks1024 <dbarker@bignerdranch.com>
2018-11-21 21:28:38 -0500

    Change Date format

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,9 +1,16 @@
 import { ALL_POSTS, MODAL_OPEN } from './types';
+import moment from 'moment';
 
 export const getAllPosts = () => {
   return (dispatch) => {
     fetch('/api/post')
     .then((response) => response.json())
+    .then((response) => {
+      return response.map((post) => {
+        post.date = moment(post.date).format('MM-DD-YYYY');
+        return post;
+      });
+    })
     .then((response) => {
       console.log(response);
       dispatch({
