09ba8f7d5adff226b58b08535686c6e9236f83ef
dbarks1024 <dbarker@bignerdranch.com>
2018-11-26 15:34:31 -0500

    fix spacing

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose
+} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
 export default function configureStore() {
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;  
- return createStore(
-  rootReducer,
-   composeEnhancers(applyMiddleware(thunk)) 
- );
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+  );
 }
