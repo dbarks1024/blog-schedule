17bf32997e658e40a81327206d2ec859d58ec858
dbarks1024 <dbarker@bignerdranch.com>
2019-01-03 11:21:56 -0500

    move produciton code

--- app.js
+++ app.js
@@ -25,4 +25,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
 
+//serve static assets if in prod
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 module.exports = app;
