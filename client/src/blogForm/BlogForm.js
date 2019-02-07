5cb2e6afc1a341da24ecc26a20f22429b1d4f4dd
dbarks1024 <dbarker@bignerdranch.com>
2019-02-07 13:56:02 -0500

    Separate blog form from modal

---
+++ client/src/blogForm/BlogForm.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { STATUS_OPTIONS, CATEGORY_OPTIONS } from '../consts';
+import { changeTitle, changeAuthor, changeStatus, changeCategory, changeDescription, changeDate } from '../postForm/_actions';
+import LoadingSpinner from '../components/spinner/LoadingSpinner';
+
+
+class BlogForm extends Component {
+
+  renderOptions(options) {
+    console.log(options);
+    return options.map((item) => {
+      console.log(item);
+      return (
+        <option key={item}>{item}</option>
+      );
+    });
+  }
+
+  isLoading = () => {
+    if(this.props.loading) {
+      return <LoadingSpinner />;
+    }else {
+      return 'Submit';
+    }
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    switch (name) {
+    case 'title':
+      this.props.changeTitle(value);
+      break;
+    case 'author':
+      this.props.changeAuthor(value);
+      break;
+    case 'date':
+      this.props.changeDate(value);
+      break;
+    case 'status':
+      this.props.changeStatus(value);
+      break;
+    case 'category':
+      this.props.changeCategory(value);
+      break;
+    case 'description':
+      this.props.changeDescription(value);
+      break;
+    default:
+      break;
+    }
+  }
+
+  render() { 
+    return ( 
+      <Form>
+        <FormGroup>
+          <Label for='title'>Title</Label>
+          <Input type='text' id='title' name='title' value={this.props.title} required onChange={this.handleInputChange}></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for='author'>Author</Label>
+          <Input type='text' id='author' name='author' value={this.props.author} required onChange={this.handleInputChange}></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for='date'>Date</Label>
+          <Input type='date' id='date' name='date' value={this.props.date} required onChange={this.handleInputChange}></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for='status'>Status</Label>
+          <Input type='select' id='status' name='status' value={this.props.status} required onChange={this.handleInputChange}>
+            <option disabled selected value=''> -- select an option -- </option>
+            {this.renderOptions(STATUS_OPTIONS)}
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for='category'>Category</Label>
+          <Input type='select' id='category' name='category' value={this.props.category} required onChange={this.handleInputChange} >
+            <option disabled selected value=''> -- select an option -- </option>
+            {this.renderOptions(CATEGORY_OPTIONS)}
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Label for='description'>Description</Label>
+          <Input type='textarea' id='description' name='description' value={this.props.description} required onChange={this.handleInputChange}></Input>
+        </FormGroup>
+        <Button onClick={this.props.handleSubmit}>
+          {this.isLoading()}
+        </Button>
+        {this.props.renderDelete ? this.props.renderDelete() : null}
+      </Form>
+    );
+  }
+}
+
+BlogForm.propTypes = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+  date: PropTypes.string,
+  status: PropTypes.string,
+  category: PropTypes.string,
+  description: PropTypes.string,
+  id: PropTypes.string,
+  loading: PropTypes.bool,
+  handleSubmit: PropTypes.func,
+  renderDelete: PropTypes.func,
+  changeTitle: PropTypes.func,
+  changeAuthor: PropTypes.func,
+  changeStatus: PropTypes.func,
+  changeCategory: PropTypes.func,
+  changeDescription: PropTypes.func,
+  changeDate: PropTypes.func,
+};
+
+const mapStateToProps = (state) => {
+  const { title, author, date, status, category, description, id, loading } = state.postForm;
+  return {
+    title,
+    author,
+    date,
+    status,
+    category,
+    description,
+    id,
+    loading
+  };
+};
+ 
+export default connect(mapStateToProps, 
+  {
+    changeTitle, 
+    changeAuthor, 
+    changeStatus, 
+    changeCategory, 
+    changeDescription, 
+    changeDate
+  })(BlogForm);
--- client/src/postForm/EditPostModal.js
+++ client/src/postForm/EditPostModal.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { changeTitle, changeAuthor, changeStatus, changeCategory, changeDescription, changeDate, submitPostForm, deletePost } from './_actions';
-import LoadingSpinner from '../components/spinner/LoadingSpinner';
-import { STATUS_OPTIONS, CATEGORY_OPTIONS } from '../consts';
+import { submitPostForm, deletePost } from './_actions';
+import BlogForm from '../blogForm/BlogForm';
 
 class EditPostModal extends Component {
 
@@ -25,101 +24,22 @@ class EditPostModal extends Component {
     this.props.deletePost();
   }
 
-  handleInputChange = (event) => {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    switch (name) {
-    case 'title':
-      this.props.changeTitle(value);
-      break;
-    case 'author':
-      this.props.changeAuthor(value);
-      break;
-    case 'date':
-      this.props.changeDate(value);
-      break;
-    case 'status':
-      this.props.changeStatus(value);
-      break;
-    case 'category':
-      this.props.changeCategory(value);
-      break;
-    case 'description':
-      this.props.changeDescription(value);
-      break;
-    default:
-      break;
-    }
-  }
-
-  isLoading = () => {
-    if(this.props.loading) {
-      return <LoadingSpinner />;
-    }else {
-      return 'Submit';
-    }
-  }
-
   renderDelete = () => {
     if(this.props.id) {
       return <Button className='btn-danger ml-3' onClick={this.handleDelete}>Delete</Button>;
     }
   }
-  
-  renderOptions(options) {
-    console.log(options);
-    return options.map((item) => {
-      console.log(item);
-      return (
-        <option key={item}>{item}</option>
-      );
-    });
-  }
+
 
   render() { 
+    const { modalOpen } = this.props;
     const closeBtn = <button className="close" style={{ position: 'absolute', top: '15px', right: '15px' }} onClick={this.handleModalClose}>&times;</button>;
     return ( 
-      <Modal isOpen={this.props.modalOpen} external={closeBtn}>
+      <Modal isOpen={modalOpen} external={closeBtn}>
         <ModalHeader>Test</ModalHeader>
         {closeBtn}
         <ModalBody>
-          <Form>
-            <FormGroup>
-              <Label for='title'>Title</Label>
-              <Input type='text' id='title' name='title' value={this.props.title} required onChange={this.handleInputChange}></Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for='author'>Author</Label>
-              <Input type='text' id='author' name='author' value={this.props.author} required onChange={this.handleInputChange}></Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for='date'>Date</Label>
-              <Input type='date' id='date' name='date' value={this.props.date} required onChange={this.handleInputChange}></Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for='status'>Status</Label>
-              <Input type='select' id='status' name='status' value={this.props.status} required onChange={this.handleInputChange}>
-                <option disabled selected value=''> -- select an option -- </option>
-                {this.renderOptions(STATUS_OPTIONS)}
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for='category'>Category</Label>
-              <Input type='select' id='category' name='category' value={this.props.category} required onChange={this.handleInputChange} >
-                <option disabled selected value=''> -- select an option -- </option>
-                {this.renderOptions(CATEGORY_OPTIONS)}
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for='description'>Description</Label>
-              <Input type='textarea' id='description' name='description' value={this.props.description} required onChange={this.handleInputChange}></Input>
-            </FormGroup>
-            <Button onClick={this.handleSubmit}>
-              {this.isLoading()}
-            </Button>
-            {this.renderDelete()}
-          </Form>
+          <BlogForm handleSubmit={this.handleSubmit}  />
         </ModalBody>
       </Modal>
     );
@@ -128,49 +48,22 @@ class EditPostModal extends Component {
 
 EditPostModal.propTypes = {
   modalOpen: PropTypes.bool,
-  title: PropTypes.string,
-  author: PropTypes.string,
-  date: PropTypes.string,
-  status: PropTypes.string,
-  category: PropTypes.string,
-  description: PropTypes.string,
   id: PropTypes.string,
-  loading: PropTypes.bool,
   setModalOpen: PropTypes.func,
-  changeTitle: PropTypes.func,
-  changeAuthor: PropTypes.func,
-  changeStatus: PropTypes.func,
-  changeCategory: PropTypes.func,
-  changeDescription: PropTypes.func,
-  changeDate: PropTypes.func,
   submitPostForm: PropTypes.func,
   deletePost: PropTypes.func,
   clearForm: PropTypes.func,
 };
 
 const mapStateToProps = (state) => {
-  const { modalOpen } = state.postForm;
-  const { title, author, date, status, category, description, id, loading } = state.postForm;
+  const { id, modalOpen } = state.postForm;
   return {
     modalOpen,
-    title,
-    author,
-    date,
-    status,
-    category,
-    description,
     id,
-    loading
   };
 };
  
 export default connect(mapStateToProps, 
-  { changeTitle, 
-    changeAuthor, 
-    changeStatus, 
-    changeCategory, 
-    changeDescription, 
-    changeDate,
-    submitPostForm,
+  { submitPostForm,
     deletePost,
   })(EditPostModal);
